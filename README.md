fd5e2458c97849e15885d4dee9b8382d1021523f
AnjolaA <anjola.awofisoye@sky.uk>
2017-01-19 20:58:26 +0000

    added files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # newman-wrapper
 A wrapper to inject config values postman environment variables
+
+## To  update environment variables
+    node ./node_modules/newman-wrapper/index.js "C:\\example\\env.json" "key" "value" 
---
+++ index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+
+if(process.argv[3] ="clientsecret"){
+var fileName = process.argv[2]
+var file = require(fileName);
+ReplaceKey(file, process.argv[3], process.argv[4]);
+
+fs.writeFile(fileName, JSON.stringify(file), function (err) {
+  if (err) return console.log(err);
+});
+}
+
+function ReplaceKey(object, keytoassign, valuetoassign) {
+  for (var x in object) {
+    if (object.hasOwnProperty(x)) {
+      if (typeof object[x] == 'object') {
+        var array = object[x];
+          for(var i in array){
+          var key = array[i].key;
+          var value = array[i].value;
+          if(key == keytoassign ){
+          array[i].value = valuetoassign;
+          }
+        }
+      }
+    }
+  }
+  console.log("Completed");
+}
+module.export = ReplaceKey;
